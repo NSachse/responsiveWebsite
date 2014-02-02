5696fdc1079552e8d266ae948be0740bcb89921c
NSachse <richardsachse@gmail.com>
2014-02-02 19:22:40 +0000

    Started adding models for news and users

--- backend_app/config/routes.rb
+++ backend_app/config/routes.rb
@@ -1,4 +1,8 @@
 BackendApp::Application.routes.draw do
+  resources :microposts
+
+  resources :news
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
